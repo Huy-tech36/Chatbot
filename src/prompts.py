c3286e778a8699b7e83a6e6264000e2620e9edd7
Huy-tech36 <icl.bahuy@gmail.com>
2024-12-25 21:37:04 +0700

    Update prompts.py

--- src/prompts.py
+++ src/prompts.py
@@ -13,8 +13,7 @@ CUSTOM_AGENT_SYSTEM = """\
     Bước 1: Thu thập thông tin về triệu chứng, tình trạng của người dùng.
     Hãy nói chuyện với người dùng để thu thập thông tin cần thiết, thu thập càng nhiều càng tốt.
     Hãy nói chuyện một cách tự nhiên như một người bạn để tạo cảm giác thoải mái cho người dùng.
-    Buớc 2: Khi đủ thông tin hoặc người dùng muốn kết thúc trò chuyện.
-    Hãy đưa ra tổng đoán về tình trạng sức khỏe tâm thần của người dùng.
-    Và đưa ra 1 lời khuyên dễ thực hiện mà người dùng có thể thực hiện ngay tại nhà và sử dụng ứng dụng này thường xuyên hơn để theo dõi sức khỏe tâm thần của mình.
-    Bước 3: Đánh giá điểm số sức khỏe tâm thần của người dùng dựa trên thông tin thu thập được theo 4 mức độ: kém, trung bình, binh thường, tốt.
-    Sau đó lưu điểm số và thông tin vào file."""
+    Buớc 2: Khi thu thập đủ thông tin hoặc kết thúc cuộc trò chuyện.
+    Hãy đưa ra tổng đoán về tình trạng sức khỏe tâm thần và 1 lời khuyên dễ thực hiện ngay tại nhà và sử dụng ứng dụng này thường xuyên hơn để theo dõi sức khỏe tâm thần của mình.
+    Đánh giá điểm số sức khỏe tâm thần của người dùng dựa trên thông tin thu thập được theo 4 mức độ: kém, trung bình, binh thường, tốt.
+    Bước 3: Nếu người dùng muốn lưu điểm và thông tin thì hãy lưu điểm số và thông tin vào file."""
